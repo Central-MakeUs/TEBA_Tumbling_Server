a0c265acc07ab474fc5f7968384cc30878643c67
Ubuntu <ubuntu@ip-172-31-35-106.ap-northeast-2.compute.internal>
2021-05-29 14:05:19 +0000

    add donateAction API

--- src/app/Donate/donateDao.js
+++ src/app/Donate/donateDao.js
@@ -3,14 +3,22 @@ async function getDonateStores(connection, param) {
                 select idx, storeName, location, possibleCount, (6371*acos(cos(radians(?))*cos(radians(latitude))*cos(radians(longitude)-radians(?))+sin(radians(?))*sin(radians(latitude))))
                     as distance
                 from Store
+                where status='가능'
                 order by distance;
                 `;
     const [storeRows] = await connection.query(getDonateStoresQuery, param);
     return storeRows;
 }
 
+async function donateAction(connection, param){
+    const donateInsertQuery = `
+        insert Donate(storeIdx, userIdx, quantity) values(?, ?, ?);
+    `;
+    const [rows] = await connection.query(donateInsertQuery, param);
+    return rows;
+}
 
 module.exports = {
     getDonateStores,
-
+    donateAction,
 };
