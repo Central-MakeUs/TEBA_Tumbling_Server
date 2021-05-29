d516edb144a7d2321e86c722a16e77f745dbd0ba
Ubuntu <ubuntu@ip-172-31-35-106.ap-northeast-2.compute.internal>
2021-05-29 13:09:22 +0000

    edit getDonateStores

--- src/app/Donate/donateRoute.js
+++ src/app/Donate/donateRoute.js
@@ -2,6 +2,6 @@ module.exports = function(app){
     const donate = require('./donateController');
     const jwtMiddleware = require('../../../config/jwtMiddleware');
 
-    app.get('/stores/donate', donate.getDonateStores);
+    app.get('/donates', donate.getDonateStores);
 
 };
