ed2390e01eec35208a4eae9e983b71e48974c466
root <root@ip-172-31-35-106.ap-northeast-2.compute.internal>
2021-05-29 14:05:54 +0000

    Add 카카오 로그인

--- src/app/User/userDao.js
+++ src/app/User/userDao.js
@@ -1,14 +1,14 @@
 // userId 회원 조회
 async function selectUserId(connection, userId) {
-  const selectUserIdQuery = `select idx from User where userNumber=?`;
-  const [userRow] = await connection.query(selectUserIdQuery, userId);
+  const selectUserIdQuery = `select idx from User where userNumber=?;`;
+  const userRow = await connection.query(selectUserIdQuery, [userId]);
   return userRow;
 }
 
 //회원가입
-async function signup(connection, userNumber, userEmail, profileImgUrl, userName ) {
-    const signupQuery = `INSERT INTO User(userNumber, userEmail, profileImgUrl, userName) SET (?,?,?,?)`;
-    const [userRow] = await connection.query(signupQuery, [userNumber, userEmail, profileImgUrl, userName]);
+async function signup(connection, userNumber, userEmail, profileImg, nickname ) {
+    const signupQuery = `INSERT INTO User(userNumber, userEmail, profileImg, nickname) VALUES(?,?,?,?)`;
+    const [userRow] = await connection.query(signupQuery, [userNumber, userEmail, profileImg, nickname]);
     return userRow;
   }
 
