c04384d06ad11ac593a52180ce72228ba0f0c397
root <root@ip-172-31-35-106.ap-northeast-2.compute.internal>
2021-05-29 11:26:46 +0000

    가게 조회

---
+++ src/app/Store/storeController.js
@@ -0,0 +1,65 @@
+const jwtMiddleware = require("../../../config/jwtMiddleware");
+const baseResponse = require("../../../config/baseResponseStatus");
+const {response, errResponse} = require("../../../config/response");
+const { pool } = require("../../../config/database");
+const { connect } = require("http2");
+const {emit} = require("nodemon");
+const storeDao = require("./storeDao");
+
+/**
+ * API No. 2
+ * API Name : 유저 조회 API (+ 이메일로 검색 조회)
+ * [GET] /app/stores
+ */
+exports.getStores = async function (req, res) {
+
+    /**
+     * Query String: latitude, longitude, distance
+     */
+    const {latitude, longitude} = req.query;
+    const distance = req.query.distance;
+
+
+    //TODO:validation
+    if(!distance)
+    distance = 1;
+
+    try {
+    const connection = await pool.getConnection(async (conn) => conn);
+    const storeResult = await storeDao.selectStore(
+    connection,
+    latitude, longitude, distance
+    );
+    connection.release();
+    return res.send(response(baseResponse.SUCCESS, storeResult))
+    }
+    catch (err) {
+        logger.error(`App - getstore error\n: ${err.message}`);
+        return errResponse(baseResponse.DB_ERROR);
+      }
+};
+
+exports.getStoreById = async function (req, res) {
+
+    /**
+     * Path variable : storeIdx
+     */
+
+    const storeIdx = req.params.storeIdx;
+
+    //TODO:validation
+
+    try {
+    const connection = await pool.getConnection(async (conn) => conn);
+    const storeResult = await storeDao.selectStoreById(
+    connection,
+    storeIdx
+    );
+    connection.release();
+    return res.send(response(baseResponse.SUCCESS, storeResult[0]))
+    }
+    catch (err) {
+        logger.error(`App - getstore error\n: ${err.message}`);
+        return errResponse(baseResponse.DB_ERROR);
+      }
+};
